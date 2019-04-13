b03ed9b01ee784cd952aa6387a0e3280e76b1621
Shafayet Sadi <43206214+ShafayetSadi@users.noreply.github.com>
2019-04-13 21:14:54 +0600

    Added comments

--- Searching/linear_search.py
+++ Searching/linear_search.py
@@ -1,10 +1,12 @@
 def linear_search(list, value):
+	"""This function takes a list as input and a value to find.Then linearly it searches for that value"""
 	for i in range(len(list)):
 		if list[i] == value:
-			return i
-	return -1
+			return i #Returning the index
+	return -1 #The value is not in the list
 
 if __name__ == '__main__':
 	array = [5, 1, 3, 9, 6, 0, 4, 8, 4, 7, 2]
 	value = 0
-	print(linear_search(array, value))
+	print("Expected output:", array.index(value))
+	print("function's output:", linear_search(array, value))
