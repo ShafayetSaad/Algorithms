481511a805bf849b4693bbe8e0b40284dbf6c96c
Shafayet Sadi <43206214+ShafayetSadi@users.noreply.github.com>
2019-04-14 14:36:49 +0600

    My second sorting algorithm

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(array):
+    """This function takes a list as input and sorts it by bubble sort algorithm"""
+    for i in range(len(array)):
+        for j in range(len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+if __name__ == '__main__':
+    list = [6, 1, 4, 9, 2]
+    print("Before sort:", list)
+    bubble_sort(list)
+    print("After sort:", list)
